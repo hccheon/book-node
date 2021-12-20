0d3375f30b934e550449ff2667a4b2e22cf9c30b
hccheon <92707576+hccheon@users.noreply.github.com>
2021-12-20 09:17:14 +0900

    init

---
+++ controllers/controller_book.js
@@ -0,0 +1,160 @@
+const common = require('../config/common');
+const msg = require('../value/message');
+require('date-utils');
+
+//책 모두 리스트 가져오기
+//책 ISBN, 책 이름으로 검색하기 
+
+
+//책 리스트 모두 가져오기
+async function book_get_all() {
+    let pool = common.getPool();
+    let connection = await pool.getConnection(async conn => conn);
+    try {
+
+        var query = `SELECT EXISTS (SELECT * FROM book) AS SUCCESS;`;
+        var output = await connection.query(query);
+        var result = {};
+
+
+
+        if (output[0][0].SUCCESS == 0) {
+            result = {
+                code: 500,
+                rows: 0,
+                output: "책이 없습니다."
+            }
+            return result
+        } else {
+            query = `SELECT * FROM book;`;
+            output = await connection.query(query);
+
+            result = {
+                code: 200,
+                rows: output[0].length,
+                output: output[0]
+            }
+        }
+        return result
+    } catch (err) {
+        console.error({ err: err })
+        var result = {
+            code: 500,
+            rows: -1,
+            output: err
+        }
+        return result
+    } finally {
+        connection.release();
+    }
+}
+
+//책 ISBN, 책, 저자 이름으로 검색하기(type, value)
+async function book_get_by_type(type, value) {
+    let pool = common.getPool();
+    let connection = await pool.getConnection(async conn => conn);
+
+    try {
+        //존재하는 book_list인지 확인
+        let query = `SELECT * FROM book WHERE ${connection.escape(type).substring(1,type.length+2-1)} = ${connection.escape(value)};`;
+        console.log({query});
+        let [book_list] = await connection.query(query);
+
+        if (!book_list.length) {
+            //book_list이 존재 하지 않는 경우
+            return common.successCode(0, "해당 도서가 없습니다.");
+        }
+
+        return common.successCode(book_list.length, book_list);
+
+    } catch (err) {
+        console.log({err});
+        return common.errorCode("오류");
+    } finally {
+        connection.release();
+    }
+}
+
+
+//책 추가
+async function book_create_book(title, author, publisher, isbn, link) {
+    let pool = common.getPool();
+    let connection = await pool.getConnection(async conn => conn)
+   
+
+    try {
+
+        //기존 책인지 확인
+        let query = `SELECT EXISTS (SELECT * FROM book WHERE isbn=${connection.escape(isbn)}) AS SUCCESS;`;
+        let [output] = await connection.query(query);
+        var result = {};
+        if(output[0].SUCCESS>0){
+           return common.successCode(output.length, "이미 추가된 책입니다."); 
+        }
+
+        //----------------
+        //책 추가하기 
+
+        query = `INSERT INTO book(title, author, publisher, isbn, link)
+                    VALUES(${connection.escape(title)}, ${connection.escape(author)}, ${connection.escape(publisher)}, ${connection.escape(isbn)}, ${connection.escape(link)});`;
+        console.log(query);
+        await connection.query(query);
+        
+        query = `SELECT * FROM book WHERE isbn = ${connection.escape(isbn)}` 
+
+        output = await connection.query(query);
+
+        return common.successCode(output.length, output[0])
+    } catch (err) {
+        console.error({ err: err })
+        var result = {
+            code: 500,
+            rows: -1,
+            output: "오류"
+        }
+        return result
+    } finally {
+        connection.release();
+    }
+}
+
+//책 삭제 (key=isbn)
+async function book_delete_book(isbn) {
+    let pool = common.getPool();
+    let connection = await pool.getConnection(async conn => conn)
+
+    try {
+        //존재하는 책인지 확인
+        let query = `SELECT EXISTS (SELECT * FROM book WHERE isbn = ${connection.escape(isbn)}) AS SUCCESS;;`;
+        let [output] = await connection.query(query);
+
+
+        if (output[0].SUCCESS==0) {
+            //isbn에 해당 도서가 없습니다.
+            return common.successCode(0, `${msg.code.S001}`); // 해당도서가 없습니다.
+        }
+
+        query = `DELETE FROM book WHERE isbn=${connection.escape(isbn)};`;
+        await connection.query(query);
+        return common.successCode(1, `${msg.code.S004}`); //삭제되었습니다.
+
+    } catch (err) {
+        return common.errorCode("오류");
+    } finally {
+        connection.release();
+    }
+}
+
+
+
+
+
+module.exports = {
+    book_get_all,
+    book_get_by_type,
+    book_create_book,
+    book_delete_book,
+
+
+}
+
---
+++ routes/book.js
@@ -0,0 +1,163 @@
+const express = require('express');
+const controller = require('../controllers/controller_book');
+const common = require('../config/common');
+const msg = require('../value/message');
+const router = express.Router();
+
+/**
+ * C PUT
+ * R GET 전체, 검색은 isbm, 책제목 또는 
+ * U POST
+ * D DELETE 
+ */
+
+//책 리스트 가져오기 
+router.get('/', common.ipfilter(common.ips, {mode: 'allow'}), async (req, res) => {
+    try{
+        let out = await controller.book_get_all();
+        res.send(out);
+        return;
+    } catch (err) {
+        
+        console.log({err:err});
+    }
+});
+
+//책 조건 검색
+router.get('/:type/:value', common.ipfilter(common.ips, {mode: 'allow'}), async (req, res) => {
+    try{
+        console.log({req_params:req.params})
+        var type = req.params.type;
+        var value = req.params.value;
+        var chk_params_ok = false;
+
+        if( ((type=="isbn") || (type=='title') || (type=='author') || (type=='publisher'))){
+            chk_params_ok = true;
+        }
+        if(!chk_params_ok){
+            res.send({
+                code:200,
+                rows:-1,
+                output: msg.code.S005
+            })
+            return;
+        }
+
+        let out = await controller.book_get_by_type(type, value);
+        res.send(out);
+        return;
+    } catch (err) {
+        
+        console.log({err:err});
+    }
+});
+
+//책 추가
+router.post('/', common.ipfilter(common.ips, {mode: 'allow'}), async (req, res)=>{
+    console.log({req_body: req.body});
+    
+    try{
+        let data = req.body,
+            result = {};
+
+        if(
+            (data.title==undefined) || (data.author==undefined) || (data.publisher==undefined) ||
+            (data.isbn==undefined) || (data.link==undefined)  
+        ){
+            res.send(common.successCode(0,"파라미터를 확인해주세요."));
+            return;
+        }else{
+            //missing
+            if(valid.missingValidation(data)){
+                result = {
+                    code: 500,
+                    rows: 0,
+                    output: 'missing값을 확인하세요.'
+                }
+                res.send(result);
+                return;
+            }
+        }
+
+        let out = await controller.book_create_book(data.title, data.author, data.publisher, data.isbn, data.link);
+
+        res.send(out);
+        return;
+    }catch(err){
+        console.log({err:err});
+    }
+});
+
+//책 삭제(with isbn)
+router.delete('/', common.ipfilter(common.ips, {mode: 'allow'}), async (req, res)=>{
+    console.log({req_body: req.body});
+    try{
+        let data = req.body;
+
+        if(data.isbn == undefined || data.isbn =="") { // 파라미터값이 undefined 경우
+            res.send(common.errorCode(msg.code.S002)); 
+            return;
+        }
+
+        if(valid.missingValidation(data)) { // 파라미터값이 null 일 경우
+            res.send(common.errorCode(msg.code.S003)); 
+            return;
+        }
+
+        let out = await controller.book_delete_book(data.isbn);
+        res.send(out);
+        return;
+    }catch(err){
+        console.log({err:err});
+    }
+});
+
+//연구 정보 수정(all columns)
+router.post('/changeAll', common.ipfilter(common.ips, {mode: 'allow'}), async (req, res)=>{
+    console.log({req_body: req.body});
+    try{
+        let data = req.body;
+
+        if((data.PRTNO==undefined) || (data.TITLE==undefined) || (data.SPONSORID==undefined) || (data.SAPPL==undefined) || (data.SSEX==undefined) || (data.STARGET==undefined) || (data.SACTIVE==undefined) || (data.SNUM==undefined)) {
+            res.send(common.errorCode('입력값을 확인하세요.'));
+            return;
+        }
+
+        if(valid.missingValidation(data)) {
+            res.send(common.errorCode('missing값을 확인하세요.'));
+            return;
+        }
+
+        let out = await controller.study_change_allColumns(data.PRTNO, data.TITLE, data.SPONSORID, data.SAPPL, data.SSEX, data.STARGET, data.SACTIVE, data.SNUM);
+        res.send(out);
+        return;
+    }catch(err){
+        console.log({err:err});
+    }
+});
+
+//특정 연구자가 참여한 모든 연구 가져오기 (with INVMAIL)
+router.post('/getStudiesOfInvestigator', common.ipfilter(common.ips, {mode: 'allow'}), async (req, res)=>{
+    console.log({req_body: req.body});
+    try{
+        let data = req.body;
+
+        if(data.INVMAIL == undefined) {
+            res.send(common.errorCode('입력값을 확인하세요.'));
+            return;
+        }
+
+        if(valid.missingValidation(data)) {
+            res.send(common.errorCode('missing값을 확인하세요.'));
+            return;
+        }
+
+        let out = await controller.study_getStudiesOfInvestigator(data.INVMAIL);
+        res.send(out);
+        return;
+    }catch(err){
+        console.log({err:err});
+    }
+});
+
+module.exports = router;
